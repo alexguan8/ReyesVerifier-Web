ff4ff83007b453291844b39b361fc288618dfd3e
Guan Alex <AGuan@reyesholdings.com>
2019-07-30 16:55:16 -0500

    Delete js password prompt

--- app/static/js/settings.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    var return_value = prompt("Password:");
-    if (return_value === "reyespassword") {
-        //load page
-    } else {
-        //redirect back to home
-        window.location.href = "/index";
-    }
-});
